08038527ab1b5f7b3cc8490a1e5db26ac814bffc
peter max <petermax2004@gmail.com>
2019-06-21 15:20:41 +0300

    Add the configuration classes and the html files

--- app/views.py
+++ app/views.py
@@ -7,4 +7,14 @@ def index():
     """
     View root function returns data in the index page
     """
-    return render_template('index.html')
+    message = 'NEWS'
+    return render_template('index.html',message = message)
+
+
+
+@app.route("/news/<int:news_id>")
+def news(news_id):
+    """
+    View news function returns details in the news details page
+    """
+    return render_template(news.html,id = news_id)
