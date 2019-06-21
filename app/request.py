a8d8fc6fae925d2dd44cbd15c985a19b52f2c319
peter max <petermax2004@gmail.com>
2019-06-21 18:17:18 +0300

    Add the process results function

--- app/request.py
+++ app/request.py
@@ -1,4 +1,53 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 #getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            movie_results = process_results(news_results_list)
+
+
+    return news_results
+
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Object news
+
+    Returns :
+        news_results: A list of movie objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        author = news_item.get('author')
+        title = news_item.get('title')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        publishedAt = news_item('publishedAt')
+        if description:
+            news_object = News(id,name,author,title,description,url,publishedAt)
+            news_results.append(news_object)
+
+    return news_results
