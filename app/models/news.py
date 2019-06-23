a13925003e757058ab9dc95e38a55ddde64c8f1a
peter max <petermax2004@gmail.com>
2019-06-23 22:22:16 +0300

    Add the classes to make the news viewable

--- app/models/news.py
+++ app/models/news.py
@@ -1,6 +1,47 @@
 class Business:
     """
-    Class to define arrangement of the business news
+    Class to define the display of the business news
+    """
+    def __init__(self,name,author,title,description,url,urlToImage,publishedAt):
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        
+class Headlines:
+    """
+    Class to define the display of the major headlines
+    """
+    def __init__(self,name,author,title,description,url,urlToImage,publishedAt):
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        
+class Sources:
+    """
+    Class define the display of all the news sources available
+    """
+    def __init__(self, id, name, url, country, description):
+        """
+        This method allows us to instantiate an instance.
+        """
+        self.id = id
+        self.name = name
+        self.url = url
+        self.country = country
+        self.description = description
+
+
+class Everything:
+    """
+    Class to define the display of all the news
     """
     def __init__(self,name,author,title,description,url,urlToImage,publishedAt):
         self.name = name
