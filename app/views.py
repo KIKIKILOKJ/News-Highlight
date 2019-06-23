8a67b2e794825c6aefdc7ad90866c53b8e3f552c
peter max <petermax2004@gmail.com>
2019-06-23 23:31:18 +0300

    Make changes to the app routes

--- app/views.py
+++ app/views.py
@@ -8,16 +8,19 @@ def index():
     """
     View root function returns data in the index page
     """
-    
+    all_news_sources = get_news_sources()
+    everything_news_items = get_everything_news()
+    business_headlines = get_business_headlines()
     title = 'HOME - WELCOME TO WHERE KNOWLEDGE IS POWER'
-    return render_template('index.html',title = title,)
+    return render_template('index.html',title = title,sources = all_news_sources,featured = everything_news_items,business = business_headlines)
 
 
 
-@app.route("/news/<news_id>")
-def news(news_id):
+@app.route("/source/<source>")
+def headlines(source):
     """
-    View news function returns details in the news details page
+    View source function returns details in the news details page
     """
+    news_headlines = get_news_headlines(source)
     title = 'HAVEN OF NEWS WORLDWIDE'
-    return render_template('news.html',id = news_id,title = title)
+    return render_template('news.html',title = title, headlines = news_headlines)
